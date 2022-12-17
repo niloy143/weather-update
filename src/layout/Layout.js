00f06c7bb95a1764d30fdc4cf30c4980782ac13b
niloy143 <niloymahmudapu@gmail.com>
2022-12-18 01:19:48 +0600

    tab

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { NavLink, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    const { pathname } = useLocation();
-
     return (
         <div className='rounded-lg shadow-lg max-w-5xl mx-3 lg:mx-auto my-12 bg-gray-300'>
-            <div className="tabs">
+            <div className="tabs pt-1 justify-center">
+                <span></span>
                 <NavLink to="/weather" className={({ isActive }) => `tab tab-lg tab-lifted ${isActive && 'tab-active'}`}>Weather</NavLink>
                 <NavLink to="/todo" className={({ isActive }) => `tab tab-lg tab-lifted ${isActive && 'tab-active'}`}>To Do</NavLink>
                 <span></span>
             </div>
-            <div className={`min-h-[50vh] p-5 bg-base-100 ${pathname === '/weather' ? 'rounded-b-lg rounded-r-lg' : 'rounded-lg'} border border-t-0 border-gray-300 relative`}>
+            <div className={`min-h-[50vh] p-5 bg-base-100 rounded-lg relative`}>
                 <Outlet />
             </div>
         </div>
