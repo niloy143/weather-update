fe0076eaae079ce8ed04eec1b97e46f44d2cca3e
niloy143 <niloymahmudapu@gmail.com>
2022-12-17 23:26:23 +0600

    stopped working on this

--- src/tabs/weather/Weather.js
+++ src/tabs/weather/Weather.js
@@ -3,16 +3,19 @@ import Loader from '../../components/Loader';
 import capitals from './capitals.json';
 
 const Weather = () => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [capital, setCapital] = useState('Abu Dhabi');
 
     useEffect(() => {
         document.title = "Weather";
+
+        setLoading(true);
         fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_apiKey}&q=${capital}`)
             .then(res => res.json())
             .then(data => {
-                setData(data);
+                console.log(data);
+                setWeather(data);
                 setLoading(false);
             })
             .catch(err => console.log(err))
@@ -20,17 +23,21 @@ const Weather = () => {
 
     return (
         loading ? <Loader text /> : <div>
-            <div>
-                {console.log(data)}
-            </div>
-            <div className='flex justify-between items-center'>
-                <h3 className='text-center grow text-3xl font-semibold'>{data.location.country}</h3>
+
+            <div className='flex flex-col-reverse sm:flex-row justify-between items-center'>
+                <div className='grow flex justify-center items-center'>
+                    <img src={weather?.current?.condition?.icon} alt="" />
+                    <h4 className='font-semibold text-lg'>{weather?.current?.condition?.text}</h4>
+                </div>
                 <select className='select select-bordered' onChange={e => setCapital(e.target.value)}>
                     {
                         capitals.sort().map((capital, i) => <option key={i} value={capital}>{capital}</option>)
                     }
                 </select>
             </div>
+            <h2 className='text-center font-semibold text-7xl mt-12 flex justify-center'>
+                {weather?.current?.temp_c}°<span className='text-xl mt-3'>C</span>
+            </h2>
         </div>
     );
 };
